07c26e1264a16aec59efb882ce9ecca0c89d3a93
Ben Wolf <benjaminwolfsbauer@gmail.com>
2025-06-20 14:18:50 +0200

    Initial project commit

---
+++ src/main/java/io/everyonecodes/brain_tease_project_bw/BrainTeaseProjectBwApplication.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.brain_tease_project_bw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainTeaseProjectBwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainTeaseProjectBwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/everyonecodes/brain_tease_project_bw/BrainTeaseProjectBwApplicationTests.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.brain_tease_project_bw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainTeaseProjectBwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
